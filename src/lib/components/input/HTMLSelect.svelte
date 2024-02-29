ac388400d7ba9d6dce6b7d6df2c2b4d9eb6d2c3f
Udo Bauer <knackwurstking.tux@gmail.com>
2024-02-29 23:02:15 +0100

    +HTMLSelect

--- src/lib/components/input/HTMLSelect.svelte
+++ src/lib/components/input/HTMLSelect.svelte
@@ -1,9 +1,24 @@
 <script>
-    // TODO: ...
     import "ui/main.min.css";
 
     import { UI } from "ui";
+
+    /**
+     * @type {import("ui/svelte/input/html-select").SelectItem[]}
+     */
+    const listItems = [
+        { label: "List entry 1", value: "le1" },
+        { label: "List entry 2", value: "le2" },
+        { label: "List entry 3", value: "le3" },
+        { label: "List entry 4", value: "le4" },
+        { label: "List entry 5", value: "le5" },
+    ];
 </script>
 
-<UI.Text.Label primary="HTMLSelect" secondary="UI.Input.HTMLSelect ..." row
-></UI.Text.Label>
+<UI.Text.Label primary="HTMLSelect" secondary="UI.Input.HTMLSelect ..." row>
+    <UI.Input.HTMLSelect
+        items={listItems}
+        selected={listItems[0] || null}
+        on:change={() => console.debug("html-select:change")}
+    />
+</UI.Text.Label>
