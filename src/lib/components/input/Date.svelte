631d0a27e35bc3930a60a096df7235390d30d66a
Udo Bauer <knackwurstking.tux@gmail.com>
2024-02-29 23:03:05 +0100

    add Date input component example

--- src/lib/components/input/Date.svelte
+++ src/lib/components/input/Date.svelte
@@ -5,4 +5,6 @@
     import { UI } from "ui";
 </script>
 
-<UI.Text.Label primary="Date" secondary="UI.Input.Date ..." row></UI.Text.Label>
+<UI.Text.Label primary="Date" secondary="UI.Input.Date ..." row>
+    <UI.Input.Date title="Pick a Date" />
+</UI.Text.Label>
