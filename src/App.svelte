a1906d008ac4c95a110c96968a78ea3928411fbf
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-01 03:20:38 +0100

    add articles for all the missing stuff

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,28 @@
 <UI.Theme.Root auto />
 
 <main class="is-max ui-container flex column align-center">
+    <article>
+        <h1>Buttons</h1>
+    </article>
+
+    <article>
+        <h1>Card</h1>
+    </article>
+
+    <article>
+        <h1>Dialog</h1>
+    </article>
+
+    <article>
+        <h1>Drawer</h1>
+    </article>
+
+    <article>
+        <h1>Flex Grid</h1>
+    </article>
+
     <article class="is-max-width">
-        <h1>Input Elements</h1>
+        <h1>Input Components</h1>
 
         <section>
             <Input.Date />
@@ -43,6 +63,22 @@
         <section>
             <Input.Text />
         </section>
+    </article>
+
+    <article>
+        <h1>Spinner</h1>
+    </article>
+
+    <article>
+        <h1>Text Components</h1>
+    </article>
+
+    <article>
+        <h1>Theme Picker</h1>
+    </article>
+
+    <article>
+        <h1>(Top) App Bar</h1>
     </article>
 </main>
 
